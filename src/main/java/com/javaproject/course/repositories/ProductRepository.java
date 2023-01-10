dccdf7330b97b5c114c4d2864ecd3bc19750b3ac
Gabriel Lira <gabrieldeio40@gmail.com>
2023-01-10 14:44:22 -0300

    Product

---
+++ src/main/java/com/javaproject/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.javaproject.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.javaproject.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+}
